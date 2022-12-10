f9004931c9878d3b1657b210a040dd24d0491c08
tnstjddlrl <48933775+tnstjddlrl@users.noreply.github.com>
2022-12-10 14:12:06 +0900

    feat: 게시글 기능까지 마무리 및 테스트 후 수정 필요

--- index.js
+++ index.js
@@ -8,11 +8,33 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  const userId = userId.client.id;
+  /** 사용자 연결시 서에 로그 표출  */
+  console.log('a user connected : ' + userId);
+
+  /** 사용자가 채팅방에 입장했을때 모든 사용자에게 입장을 알림 */
+  socket.on('enter_messageRoom', (msg) => {
+    console.log('enter the messageRoom : ' + userId);
+    console.log(JSON.stringify({ type: 'sytemMSG', name: 'Alert', chat: `새로운 사용자(${userId})가 입장했어요!` }));
+    io.emit({ type: 'sytemMSG', name: 'Alert', chat: `새로운 사용자(${userId})가 입장했어요!` });
+  });
+
   /** 사용자의 채팅 감지 */
-  socket.on('chat message', (msg) => {
+  socket.on('chat_message', (msg) => {
     console.log('chat the' + msg);
-    io.emit('chat message', msg);
+    io.emit({ type: 'userMSG', name: userId, chat: msg });
+  });
+
+  /** 사용자가 게시글 보는 페이지에 접근시 */
+  socket.on('view_first_board', (msg) => {
+    console.log('view first board' + msg);
+    socket.emit({ title: '이건 첫 게시글이에요!', body: '친구들과 소통할 게시글을 남겨보세요!', picture: [{ index: 1, img: 'https://picok.co.kr/data/file/wing8657/m15526234805252/img_m15526234805252_800.jpg' }] });
+  });
+
+  /** 사용자가 게시글 남길때 모든 사용자에게 글 전송 */
+  socket.on('write_the_board', (msg) => {
+    console.log('write_the_board' + msg);
+    io.emit({ title: '이건 첫 게시글이에요!', body: '친구들과 소통할 게시글을 남겨보세요!', picture: [{ index: 1, img: 'https://picok.co.kr/data/file/wing8657/m15526234805252/img_m15526234805252_800.jpg' }] });
   });
   /** 사용자 이탈 감지 */
   socket.on('disconnect', () => {
