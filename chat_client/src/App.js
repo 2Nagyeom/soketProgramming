20a6b26fe594e776120ac19dc68c15dc9c36829e
tnstjddlrl <48933775+tnstjddlrl@users.noreply.github.com>
2022-12-10 15:52:41 +0900

    feat: 채팅 보낼수있게 변경 및 채팅방 기능 50프로 완성

--- chat_client/src/App.js
+++ chat_client/src/App.js
@@ -1,50 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './App.css';
 import { Navbar, Nav, Container, NavDropdown, Form, Button, FormControl } from 'react-bootstrap';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { disconnectSocket, initSocketConnection, sendSocketMessage, socket } from './socket';
 
 
-function ColorSchemesExample() {
+function App() {
+
+  useEffect(() => {
+    initSocketConnection()
+    return () => {
+      disconnectSocket()
+    }
+  }, [])
 
   return (
     <div className='App'>
-			<BrowserRouter>
-				<Header />
-				<Routes>
-					<Route path="/Main" element={<Main />}></Route>
-					<Route path="/Chat" element={<Chat />}></Route>
-				</Routes>
-			</BrowserRouter>
-		</div>
-    
-    
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/Main" element={<Main />}></Route>
+          <Route path="/Chat" element={<Chat />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+
+
   );
 }
 
-export default ColorSchemesExample;
+export default App;
 
-const Main =() => {
-  return(
+const Main = () => {
+  return (
     null
-    )
+  )
 }
 
-const Chat=()=>{
-  return(
-    <>
-    <ul id="messages">
+const Chat = () => {
+  const [inputText, setInputText] = useState('')
+  useEffect(() => {
+    sendSocketMessage()
 
-    </ul>
-    <form id="form" >
-      <input id="input" autocomplete="off" /><button>Send</button>
-    </form>
+    return () => {
+    }
+  }, [])
+
+  return (
+    <>
+      <ul id="messages">
+
+      </ul>
+      <form id="form" onSubmit={(e) => {
+        e.preventDefault();
+        sendSocketMessage('chat_message', inputText);
+      }}>
+        <input id="input" autocomplete="off" value={inputText} onChange={(e) => {
+          setInputText(e.target.value)
+        }} /><button>Send</button>
+      </form>
     </>
   )
 }
 
 
 const Header = () => {
-  return(
+  return (
     <Navbar bg="primary" variant="dark">
       <Container>
         <Navbar.Brand href="">Navbar</Navbar.Brand>
