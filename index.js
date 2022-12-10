4bb40b09d0229d3ef2d085196c2b721f2b93ba0d
tnstjddlrl <48933775+tnstjddlrl@users.noreply.github.com>
2022-12-10 14:59:42 +0900

    feat: 클라이언트에서 소켓으로 연결 완료

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 io.on('connection', (socket) => {
   const userId = socket.client.id;
